b7c53cc89797e5132acfba1ac2823b18b443e3d5
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2023-12-06 10:01:54 -0700

    ChosenVariables

--- project.R
+++ project.R
@@ -1,4 +1,14 @@
 setwd("C:/Users/verti/Downloads/archive")
+setwd("c:/StatsRStudio/StatsRProject")
 
 heart <- read.csv(file="heart.csv", sep=",", header=T)
 head(heart$Age)
+
+# 2 Catigorical
+# FastingBS: fasting blood sugar [1: if FastingBS > 120 mg/dl, 0: otherwise]
+# ChestPainType: chest pain type [TA: Typical Angina, ATA: Atypical Angina, NAP: Non-Anginal Pain, ASY: Asymptomatic]
+
+# 3 Numerical
+# Age: age of the patient [years]
+# RestingBP: resting blood pressure [mm Hg]
+# MaxHR: maximum heart rate achieved [Numeric value between 60 and 202]
