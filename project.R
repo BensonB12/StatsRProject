59c8681912c41cf5836923b5e858e202b5427a86
garioncox <garionlcox@gmail.com>
2023-12-06 10:15:17 -0700

    Barplot (bp vs paintype) added

--- project.R
+++ project.R
@@ -1,8 +1,5 @@
-setwd("C:/Users/verti/Downloads/archive")
+setwd("C:/Users/verti/Downloads/r-final/StatsRProject")
 setwd("c:/StatsRStudio/StatsRProject")
-
-heart <- read.csv(file="heart.csv", sep=",", header=T)
-head(heart$Age)
 
 # 2 Catigorical
 # FastingBS: fasting blood sugar [1: if FastingBS > 120 mg/dl, 0: otherwise]
@@ -12,3 +9,13 @@ head(heart$Age)
 # Age: age of the patient [years]
 # RestingBP: resting blood pressure [mm Hg]
 # MaxHR: maximum heart rate achieved [Numeric value between 60 and 202]
+
+library(ggplot2)
+
+heart <- read.csv(file="heart.csv", sep=",", header=T)
+df <- data.frame(heart$Sex, heart$ChestPainType, heart$Age, heart$RestingBP, heart$MaxHR)
+
+head(df)
+
+ggplot(df, aes(x = heart.ChestPainType, y = heart.RestingBP)) +
+geom_bar(stat="identity")
