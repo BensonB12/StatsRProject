4d64b41b82b92a107e867c38664b589a54f8f2be
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2023-12-09 17:40:30 -0700

    ItsCrap

--- benson.R
+++ benson.R
@@ -1,60 +1,87 @@
-# Benson's File (So there are no merge conflicts)
-
 setwd("c:/StatsRStudio/StatsRProject")
+install.packages("languageserver")
+install.packages("ggplot2", dep=T)
+#install.packages("ggmosaic")
+#install.packages("devtools")
+#devtools::install_github("haleyjeppson/ggmosaic")
+library(ggplot2)
 
-heart <- read.csv(file="heart.csv", sep=",", header=T)
-df <- data.frame(heart$Sex, heart$ChestPainType, heart$Age, heart$RestingBP, heart$MaxHR, heart$HeartDisease)
-
-# (3) Catigorical
-# Sex: sex of the patient [M: Male, F: Female]
+# 2 Catigorical
+# FastingBS: fasting blood sugar [1: if FastingBS > 120 mg/dl, 0: otherwise]
 # ChestPainType: chest pain type [TA: Typical Angina, ATA: Atypical Angina, NAP: Non-Anginal Pain, ASY: Asymptomatic]
-# HeartDisease: output class [1: heart disease, 0: Normal]
+# HeartDisease: true of false (1, 0)
 
 # 3 Numerical
 # Age: age of the patient [years]
 # RestingBP: resting blood pressure [mm Hg]
 # MaxHR: maximum heart rate achieved [Numeric value between 60 and 202]
 
-library(ggplot2)
-install.packages("crayon")
+heart <- read.csv(file="heart.csv", sep=",", header=T)
 
-head(df)
+# Barplot
+ggplot(heart, aes(x = ChestPainType, y = RestingBP)) +
+  geom_bar(stat="identity", fill="steelblue") +
+  labs(y= "Resting Blood Pressure (mm Hg)", x = "Chest Pain Type") +
+  ggtitle("Residents in: Clevelend, Hungarian, \nSwitzerland, and California") +
+  scale_x_discrete(labels = c('Asymptomatic','Atypical Angina','Non-Anginal', 'Typical Angina'))
 
-ggplot(df, aes(x = heart.ChestPainType, y = heart.RestingBP)) +
-  geom_bar(stat="identity")
+# Mosaic Plot 
+mosaicplot(~heart$Sex + heart$ChestPainType,
+           xlab = "Sex",
+           ylab = "Type of Chest Pain",
+           main="Residents in: Clevelend, Hungarian, \nSwitzerland, and California")
 
-ggplot(df, aes(x = heart.ChestPainType, y = heart.Age)) + 
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.ChestPainType, y = heart.MaxHR)) + 
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.HeartDisease, y = heart.RestingBP)) +
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.HeartDisease, y = heart.Age)) +
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.HeartDisease, y = heart.MaxHR)) +
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.Sex, y = heart.RestingBP)) +
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.Sex, y = heart.Age)) +
-  geom_bar(stat="identity")
-
-ggplot(df, aes(x = heart.Sex, y = heart.MaxHR)) +
-  geom_bar(stat="identity")
-
-summary(heart$ChestPainType)
+# Stacked Bar Plot
+condition <- rep(c("Has a Heart Disease", "Does not have Heart Disease"), 459)
+ggplot(heart, aes(fill=condition, y=HeartDisease ,x=ChestPainType)) +
+  geom_bar(position="stack", stat="identity") +
+  labs(y= "Resting Blood Pressure (mm Hg)", x = "Chest Pain Type") +
+  ggtitle("Residents in: Clevelend, Hungarian, \nSwitzerland, and California") +
+  scale_x_discrete(labels = c('Asymptomatic','Atypical Angina','Non-Anginal', 'Typical Angina'))
 
 # side by side bar plot
-condition <- rep(c("has Heart Disease", "Does not have HeartDisease"), 459)
-ggplot(df, aes(fill=condition, y=heart$Age, x=heart$ChestPainType)) +
-  geom_bar(position="dodge", stat="identity")
+ggplot(heart, aes(fill=condition, y=Age, x=ChestPainType)) +
+  geom_bar(position="dodge", stat="identity") +
+  ggtitle("Residents in: Clevelend, Hungarian, \nSwitzerland, and California") +
+  scale_x_discrete(labels = c('Asymptomatic','Atypical Angina','Non-Anginal', 'Typical Angina'))
 
-# stacked bar
-ggplot(df, aes(fill=condition, y=heart$Age, x=heart$ChestPainType)) +
-  geom_bar(position="stack", stat="identity")
+# Side by Side Box + Whisker Plot
+boxplot(heart$MaxHR ~ heart$Sex)
 
+# Histogram
+hist(heart$Age)
+hist(heart$RestingBP)
+hist(heart$MaxHR)
+
+# Scatterplot with fit line
+plot(heart$Age, heart$RestingBP)
+abline(lm(heart$RestingBP ~ heart$Age, data = heart), col = "blue")
+
+t.test(heart$MaxHR ~ heart$Sex)
+
+ggplot(heart, aes(x = ChestPainType, y = RestingBP)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = ChestPainType, y = Age)) + 
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = ChestPainType, y = MaxHR)) + 
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = HeartDisease, y = RestingBP)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = HeartDisease, y = Age)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = HeartDisease, y = MaxHR)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = Sex, y = RestingBP)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = Sex, y = Age)) +
+  geom_bar(stat="identity")
+
+ggplot(heart, aes(x = Sex, y = MaxHR)) +
+  geom_bar(stat="identity")
